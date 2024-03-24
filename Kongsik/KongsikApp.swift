44c83ba4ec27e2c3f362766c0713fa76d3a6411c
iosdevSW <iosdev.sw@gmail.com>
2024-03-24 18:37:37 +0900

    Initial Commit

---
+++ Kongsik/KongsikApp.swift
@@ -0,0 +1,17 @@
+//
+//  KongsikApp.swift
+//  Kongsik
+//
+//  Created by iOS신상우 on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct KongsikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
